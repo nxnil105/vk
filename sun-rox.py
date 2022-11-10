3dd1a21a5a34768b5f8a67537c574ec8b4cf964b
nxnil105 <113341774+nxnil105@users.noreply.github.com>
2022-11-10 12:17:32 +0600

    Create sun-rox.py

---
+++ sun-rox.py
@@ -0,0 +1,32 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+
+        os.system('xdg-open https://www.facebook.com/Captain.TaRikuL.420')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from base64 import Subscraption
+ 
+        Subscraption()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from base32 import Subscraption
+ 
+ 
+        Subscraption()
